b77f79c435e2bb5f435a92c221fe916b035174de
André Abreu de Sá <andre.abreu.ufmg@gmail.com>
2022-10-07 21:24:24 +0000

    finish NAVBAR

--- components/sections/Header/Header.tsx
+++ components/sections/Header/Header.tsx
@@ -1,11 +1,16 @@
 import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import { lighten } from 'polished';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import cn from 'classnames';
-import { Container, Link } from '@components/ui';
+import { Box, Container, Link } from '@components/ui';
+import { MenuButton } from './Button Humburger';
+import { MenuHumburger } from './Menu Humburger';
+import { Logo } from '@components/ui/Logo/Logo';
 //import { CloseButton } from './Close';
 
-export const Header = ({ onClose }) => {
+
+export const Header = () => {
   const { pathname } = useRouter();
   const links = [
     {
@@ -39,14 +44,18 @@ export const Header = ({ onClose }) => {
       {
         'hover:text-3xl hover:text-green-400 transition duration-300 ease-linear hover:translate-y-2' : pathname !== path,
       },
-      'font-heading md:ml-2 max-w-xs px-2 text-2xl   '
+      'font-heading md:ml-2 max-w-xs px-3 text-2xl   '
        
     );
   };
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const onOpen = () => setIsMenuOpen(true);
+  const onClose = () => setIsMenuOpen(false);
+
   return (
-    <Container className=' flex h-16 items-center justify-between  border-b-4 border-b-rose-600'>
-      <ul className='flex h-full items-center justify-center gap-5'>
+    <Container className='flex  h-16 items-center justify-between  border-b-4 border-b-rose-600'>
+      <ul className='  sm:hidden flex h-full items-center justify-center gap-1'>
         {links.map(({ label, href }, idx) => (
           <Link
             href={href}
@@ -56,7 +65,16 @@ export const Header = ({ onClose }) => {
             {label}
           </Link>
         ))}
+        
       </ul>
+      
+      <Box className='flex  '>
+        <MenuButton onOpen={onOpen} />
+        <AnimatePresence>
+          {isMenuOpen && <MenuHumburger onClose={onClose} />}
+        </AnimatePresence>
+      </Box>
+      <Logo className='flex-row' />
     </Container>
   )
 
